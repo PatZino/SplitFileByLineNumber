890c8a0c6244b57c3dd31ceef72c45b9d73f6e65
Patricia Edaware <eedawarez@gmail.com>
2022-12-18 02:08:38 +0000

    added error handling

--- SplitTextFileByLineNumber/SplitFile.cs
+++ SplitTextFileByLineNumber/SplitFile.cs
@@ -8,39 +8,51 @@ namespace SplitFileByLineNumber
 	{
         public static void Execute(string inputFilePath, int numOfLines)
         {
-            int counter = 0, totalcounter = 0, countFileGroup = 1;
-            var splitFilePath = inputFilePath.Split('\\');
-            var fileName = (splitFilePath[splitFilePath.Length - 1]).Split('.');
-            var newFileName = fileName[0] + "_";
-            var fileExtension = fileName[1];
-            var outputPath = @"C:\PProjects\TestFiles\OutputFile\{0}."+ fileExtension;
-            var newFile = new StringBuilder();
- 
-            foreach (string line in File.ReadLines(inputFilePath))
+            try 
             {
-                newFile.AppendLine(line);
-                counter++;
-                totalcounter++;
+                if (numOfLines < 1) 
+                {
+                    Console.WriteLine("Number of lines should be a value greater than 0");
+                    return;
+                }
+                int counter = 0, totalcounter = 0, countFileGroup = 1;
+                var splitFilePath = inputFilePath.Split('\\');
+                var fileName = (splitFilePath[splitFilePath.Length - 1]).Split('.');
+                var newFileName = fileName[0] + "_";
+                var fileExtension = fileName[1];
+                var outputPath = @"C:\PProjects\TestFiles\OutputFile\{0}."+ fileExtension;
+                var newFile = new StringBuilder();
+ 
+                foreach (string line in File.ReadLines(inputFilePath))
+                {
+                    newFile.AppendLine(line);
+                    counter++;
+                    totalcounter++;
 
-                if (counter == numOfLines)
+                    if (counter == numOfLines)
+                    {
+                        var outputFilePath = String.Format(outputPath, newFileName + countFileGroup);
+                        File.WriteAllText(outputFilePath, newFile.ToString());
+                        counter = 0;
+                        countFileGroup++;
+                        newFile = new StringBuilder();
+                    }
+                }
+
+                if (counter > 0)
                 {
                     var outputFilePath = String.Format(outputPath, newFileName + countFileGroup);
                     File.WriteAllText(outputFilePath, newFile.ToString());
-                    counter = 0;
-                    countFileGroup++;
-                    newFile = new StringBuilder();
                 }
-            }
 
-            if (counter > 0)
+                Console.WriteLine("There were {0} lines.", totalcounter);
+            
+                Console.ReadLine(); // Suspend the screen. 
+            }
+            catch (Exception ex)
             {
-                var outputFilePath = String.Format(outputPath, newFileName + countFileGroup);
-                File.WriteAllText(outputFilePath, newFile.ToString());
+                Console.WriteLine(ex.Message);
             }
-
-            Console.WriteLine("There were {0} lines.", totalcounter);
-
-            Console.ReadLine(); // Suspend the screen. 
         }
     }
 }
